51effa167477b811d173b841ddbd5507676f9809
Sedclerk <seclerk@gmail.com>
2013-09-01 17:53:37 +0300

    Worked script

---
+++ Test in terminal worked.rb
@@ -0,0 +1,22 @@
+Last login: Sun Sep  1 17:42:08 on console
+203:~ Sedclerk$ cd desktop
+203:desktop Sedclerk$ ruby Project14.rb
+Warning: you should require 'minitest/autorun' instead.
+Warning: or add 'gem "minitest"' before 'require "minitest/autorun"'
+From:
+  /Users/Sedclerk/.rbenv/versions/1.9.3-p392/lib/ruby/1.9.1/minitest/autorun.rb:14:in `<top (required)>'
+  Project14.rb:3:in `<main>'
+MiniTest::Unit.autorun is now Minitest.autorun. From /Users/Sedclerk/.rbenv/versions/1.9.3-p392/lib/ruby/1.9.1/minitest/autorun.rb:18:in `<top (required)>'
+MiniTest::Unit::TestCase is now Minitest::Test. From Project14.rb:27:in `<main>'
+Run options: --seed 9697
+
+# Running:
+
+.
+
+Finished in 0.001286s, 777.6050 runs/s, 5443.2348 assertions/s.
+
+1 runs, 7 assertions, 0 failures, 0 errors, 0 skips
+203:desktop Sedclerk$ 
+
+
